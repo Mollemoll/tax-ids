6bc867a77da53f9ad69f6cfab385913d426e96f3
Jonas Molander <jonas.molander@gmail.com>
2024-05-02 10:56:25 +0200

    feat: #ensure_valid_syntax on tax ids (#1)
    ### Why?
    
    As a user of TaxIds I want to make as few government database lookups as
    possible. By making sure that the TaxId initializer only creates
    instances that adhere to the specific TaxId type syntaxes we can avoid
    any unnecessary lookups.
    
    ### What changed?
    
    - Added the `tax_country_code` field to the TaxId struct. This field is
    populated with the two-letter code at the start of the tax id value. IE:
    `"SE"` in `"SE123456789101"`. This was added due to cases where the
    TaxId value uses another two-letter iso code than the country. IE:
    `"GR"` & `"EL"` or `"GB"` and `"XI"`.
    - Depending on the tax id value provided, TaxId initializer will route
    tp different `TaxIdTypes`. IE: `"SE123456789101"` will resolve to the
    `EUVat` while `"GBHA123"` will resolve to `GBVat`.
    - Added regexes for EUVats & GBVat

--- src/tax_id.rs
+++ src/tax_id.rs
@@ -1,34 +1,44 @@
+use std::process::id;
 use crate::eu_vat::EUVat;
 use crate::gb_vat::GBVat;
 use crate::errors::ValidationError;
+use crate::eu_vat;
 
 pub trait TaxIdType {
     fn name(&self) -> &'static str;
+    fn ensure_valid_syntax(&self, value: &str) -> bool;
+    fn country_code_from(&self, tax_country_code: &str) -> String;
 }
 
 struct TaxId {
     value: String,
     country_code: String,
+    tax_country_code: String,
     local_value: String,
     id_type: &'static str,
 }
 
 impl TaxId {
     pub fn new(value: &str) -> Result<TaxId, ValidationError> {
-        let country_code = &value[0..2];
+        let tax_country_code = &value[0..2];
         let local_value = &value[2..];
-        let id_type: Box<dyn TaxIdType> = match country_code {
-            "SE" => Box::new(EUVat),
+
+        let id_type: Box<dyn TaxIdType> = match tax_country_code {
             "GB" => Box::new(GBVat),
-            _ => return Err(ValidationError::new("Unknown country code"))
+            _ if eu_vat::COUNTRIES.contains(&tax_country_code) => Box::new(EUVat),
+            _ => return Err(ValidationError::new("Unknown country code")),
         };
 
-        Ok(TaxId {
-            id_type: id_type.name(),
-            value: value.to_string(),
-            country_code: country_code.to_string(),
-            local_value: local_value.to_string(),
-        })
+        match id_type.ensure_valid_syntax(value) {
+            false => Err(ValidationError::new("Invalid syntax")),
+            true => Ok(TaxId {
+                id_type: id_type.name(),
+                value: value.to_string(),
+                tax_country_code: tax_country_code.to_string(),
+                country_code: id_type.country_code_from(tax_country_code),
+                local_value: local_value.to_string(),
+            })
+        }
     }
 
     pub fn value(&self) -> &str { &self.value }
@@ -43,25 +53,49 @@ mod tests {
 
     #[test]
     fn test_new_eu_vat() {
-        let tax_id= TaxId::new("SE1234567890").unwrap();
-        assert_eq!(tax_id.value(), "SE1234567890");
+        let tax_id= TaxId::new("SE123456789101").unwrap();
+        assert_eq!(tax_id.value(), "SE123456789101");
         assert_eq!(tax_id.country_code(), "SE");
-        assert_eq!(tax_id.local_value(), "1234567890");
+        assert_eq!(tax_id.local_value(), "123456789101");
+        assert_eq!(tax_id.id_type(), "eu_vat");
+    }
+
+    #[test]
+    fn test_new_gr_vat() {
+        let tax_id = TaxId::new("EL123456789").unwrap();
+        assert_eq!(tax_id.value(), "EL123456789");
+        assert_eq!(tax_id.country_code(), "GR");
+        assert_eq!(tax_id.local_value(), "123456789");
         assert_eq!(tax_id.id_type(), "eu_vat");
     }
 
     #[test]
     fn test_new_gb_vat() {
-        let tax_id = TaxId::new("GB123456789").unwrap();
-        assert_eq!(tax_id.value(), "GB123456789");
+        let tax_id = TaxId::new("GB591819014").unwrap();
+        assert_eq!(tax_id.value(), "GB591819014");
         assert_eq!(tax_id.country_code(), "GB");
-        assert_eq!(tax_id.local_value(), "123456789");
+        assert_eq!(tax_id.local_value(), "591819014");
         assert_eq!(tax_id.id_type(), "gb_vat");
+    }
+
+    #[test]
+    fn test_new_xi_vat() {
+        let tax_id = TaxId::new("XI591819014").unwrap();
+        assert_eq!(tax_id.value(), "XI591819014");
+        assert_eq!(tax_id.country_code(), "GB");
+        assert_eq!(tax_id.local_value(), "591819014");
+        assert_eq!(tax_id.id_type(), "eu_vat");
     }
 
     #[test]
     #[should_panic(expected = "Unknown country code")]
     fn test_new_unknown_country_code() {
         let _ = TaxId::new("XX123456789").unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid syntax")]
+    fn test_failed_validation() {
+        let _ = TaxId::new("SE12").unwrap();
     }
 }
