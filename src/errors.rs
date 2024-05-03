20cfb7a74b762cd941e63d55dafb3cb4e129edef
Mollemoll <jonas.molander@gmail.com>
2024-05-03 09:36:48 +0200

    feat: verifiers and verifications

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::fmt;
 
 #[derive(Debug)]
@@ -19,4 +20,28 @@ impl fmt::Display for ValidationError {
     }
 }
 
-impl std::error::Error for ValidationError {}
+impl Error for ValidationError {}
+
+#[derive(Debug)]
+pub enum VerificationError {
+    HttpError(reqwest::Error),
+    ParsingError(roxmltree::Error),
+}
+
+impl fmt::Display for VerificationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            VerificationError::HttpError(err) => write!(f, "HTTP client error: {}", err),
+            VerificationError::ParsingError(err) => write!(f, "Parsing error: {}", err),
+        }
+    }
+}
+
+impl Error for VerificationError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            VerificationError::HttpError(err) => Some(err),
+            VerificationError::ParsingError(err) => Some(err),
+        }
+    }
+}
---
+++ src/verification.rs
@@ -0,0 +1,74 @@
+use chrono::prelude::*;
+use crate::tax_id::TaxId;
+use crate::errors::VerificationError;
+
+#[derive(Debug, PartialEq)]
+pub enum VerificationStatus {
+    Verified,
+    Unverified,
+    Unavailable,
+}
+
+
+pub struct Verification {
+    performed_at: DateTime<Local>,
+    status: VerificationStatus,
+}
+
+impl Verification {
+    pub fn new(status: VerificationStatus) -> Verification {
+        Verification {
+            performed_at: Local::now(),
+            status,
+        }
+    }
+
+    pub fn status(&self) -> &VerificationStatus { &self.status }
+}
+
+pub trait Verifier {
+    async fn verify(&self, tax_id: TaxId) -> Result<Verification, VerificationError> {
+        let request = self.make_request(tax_id).await?;
+        let verification = self.parse_response(request)?;
+        Ok(verification)
+    }
+
+    async fn make_request(&self, tax_id: TaxId) -> Result<String, VerificationError>;
+
+    fn parse_response(&self, _response: String) -> Result<Verification, VerificationError>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_verification() {
+        let verification = Verification::new(VerificationStatus::Verified);
+        assert_eq!(*verification.status(), VerificationStatus::Verified);
+        assert_eq!(verification.performed_at.date_naive(), Local::now().date_naive());
+    }
+
+    struct TestVerifier;
+
+    impl Verifier for TestVerifier {
+        async fn make_request(&self, _tax_id: TaxId) -> Result<String, VerificationError> {
+            Ok("test".to_string())
+        }
+
+        fn parse_response(&self, response: String) -> Result<Verification, VerificationError> {
+            if response == "test" {
+                Ok(Verification::new(VerificationStatus::Verified))
+            } else { panic!("Unexpected response") }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_verify() {
+        let tax_id = TaxId::new("SE123456789101").unwrap();
+        let verifier = TestVerifier;
+        let verification = verifier.verify(tax_id).await.unwrap();
+        assert_eq!(*verification.status(), VerificationStatus::Verified);
+        assert_eq!(verification.performed_at.date_naive(), Local::now().date_naive());
+    }
+}
